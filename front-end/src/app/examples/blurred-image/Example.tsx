b447ed9e5048d81dd66dfeef344ba240100d6060
Oleksii Popov <alexejpopovua@gmail.com>
2024-08-18 11:43:18 +0200

    feat(front-end): add the blurred-image example

--- front-end/src/app/examples/blurred-image/Example.tsx
+++ front-end/src/app/examples/blurred-image/Example.tsx
@@ -1,5 +1,25 @@
 import { ExampleBox } from '@/app/shared/ExampleBox';
+import { imageLoader } from '@/app/shared/image-loader';
+import Image from 'next/image';
+import React from 'react';
+import { readBlurredImageSrcPair } from '@/app/shared/image';
 
-export function Example() {
-  return <ExampleBox></ExampleBox>;
+export async function Example() {
+  const blurredImageSrcPair = await readBlurredImageSrcPair({ src: '/20231105_143110.jpg' });
+
+  return (
+    <ExampleBox>
+      <Image
+        src="/20231105_143110.jpg"
+        loader={imageLoader}
+        placeholder={'blur'}
+        blurDataURL={blurredImageSrcPair.blurDataURL}
+        alt="Picture of the author"
+        sizes="100vw"
+        className="h-auto w-full"
+        width={500}
+        height={500 / blurredImageSrcPair.ratio}
+      />
+    </ExampleBox>
+  );
 }
