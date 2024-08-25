e7859a49ffd9c4712ce3a027f503f02e19019b79
Oleksii Popov <alexejpopovua@gmail.com>
2024-08-25 16:04:33 +0200

    docs(front-end): add description to the landing page

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -9,32 +9,38 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="mb-32 grid text-center lg:mt-32 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-      <HomeNavLink
-        path="/examples/responsive-image/"
-        label={Messages.responsiveImage.title}
-        description={Messages.responsiveImage.description}
-      />
-      <HomeNavLink
-        path="/examples/thumbnails/"
-        label={Messages.thumbnails.title}
-        description={Messages.thumbnails.description}
-      />
-      <HomeNavLink
-        path="/examples/image-fit/"
-        label={Messages.imageFit.title}
-        description={Messages.imageFit.description}
-      />
-      <HomeNavLink
-        path="/examples/blurred-image/"
-        label={Messages.blurredImage.title}
-        description={Messages.blurredImage.description}
-      />
-      <HomeNavLink
-        path="/examples/blurred-animated-image/"
-        label={Messages.blurredAnimatedImage.title}
-        description={Messages.blurredAnimatedImage.description}
-      />
-    </div>
+    <>
+      <div className="py-16 text-center text-2xl">
+        This is a simple demo web application showcasing the integration of the AWS Serverless Image Handler with a
+        statically exported Next.js website
+      </div>
+      <div className="mb-32 grid text-center lg:mt-32 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <HomeNavLink
+          path="/examples/responsive-image/"
+          label={Messages.responsiveImage.title}
+          description={Messages.responsiveImage.description}
+        />
+        <HomeNavLink
+          path="/examples/thumbnails/"
+          label={Messages.thumbnails.title}
+          description={Messages.thumbnails.description}
+        />
+        <HomeNavLink
+          path="/examples/image-fit/"
+          label={Messages.imageFit.title}
+          description={Messages.imageFit.description}
+        />
+        <HomeNavLink
+          path="/examples/blurred-image/"
+          label={Messages.blurredImage.title}
+          description={Messages.blurredImage.description}
+        />
+        <HomeNavLink
+          path="/examples/blurred-animated-image/"
+          label={Messages.blurredAnimatedImage.title}
+          description={Messages.blurredAnimatedImage.description}
+        />
+      </div>
+    </>
   );
 }
